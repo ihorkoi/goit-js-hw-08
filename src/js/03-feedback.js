175378c6adcc4f10c5664e7181722a4bf5f85ba3
ihorkoi <koleev.igor@gmail.com>
2023-06-21 16:11:19 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,26 @@
 const throttle = require('lodash.throttle');
 
+const FORM_KEY = 'feedback-form-state'
 
 const formRef = document.querySelector('.feedback-form');
 
-if (localStorage['feedback-form-state']) {
-    formRef.email.value = JSON.parse(localStorage['feedback-form-state']).email;
-    formRef.message.value = JSON.parse(localStorage['feedback-form-state']).message;    
+if (localStorage[FORM_KEY]) {
+    for (key of Object.keys(JSON.parse(localStorage[FORM_KEY]))) {
+       formRef[key].value = JSON.parse(localStorage[FORM_KEY])[key]
+   }
 }
 
-const formData = {};
+const formData = JSON.parse(localStorage[FORM_KEY]) || {};
 
 formRef.addEventListener('input', throttle((evt) => {
     formData[evt.target.name] = evt.target.value;
-    localStorage['feedback-form-state'] = JSON.stringify(formData);
+    localStorage[FORM_KEY] = JSON.stringify(formData);
 }),500)
 
 
 formRef.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    console.log(JSON.parse(localStorage['feedback-form-state']));
+    console.log(JSON.parse(localStorage[FORM_KEY]));
     formRef.reset();
-    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem(FORM_KEY);
 })
